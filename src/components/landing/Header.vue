45915bf35147845a722606bcbb661eb41e2c4113
exe123deadstart <valencia.exequiel123@gmail.com>
2025-08-08 00:10:30 +0900

    Updated heaer

--- src/components/landing/Header.vue
+++ src/components/landing/Header.vue
@@ -4,14 +4,15 @@
       <v-img class="mb-4" height="300" src="@/assets/zik-logo.svg" />
       <v-col cols="12">
         <v-btn
-          v-for="item in ['Home','Skills', 'Experience', 'Projects', 'More About Me','Contact']"
-          :key="item"
-          class="mx-2 text-h5"
+          v-for="item in navItems"
+          :key="item.label"
+          class="mx-2 text-h4"
+          :href="`#${item.id}`"
           size="x-large"
-          :to="`/${item.toLowerCase()}`"
           variant="text"
+          @click.prevent="scrollTo(item.id)"
         >
-          {{ item }}
+          {{ item.label }}
         </v-btn>
       </v-col>
 
@@ -22,7 +23,22 @@
 </template>
 
 <script lang="ts" setup>
-  //
+  import { ref } from 'vue'
+
+  const navItems = ref([
+    { label: 'Home', id: 'home' },
+    { label: 'About', id: 'about' },
+    { label: 'Skills', id: 'skills' },
+    { label: 'Experience', id: 'experience' },
+    { label: 'Contact', id: 'contact' },
+  ])
+
+  function scrollTo (id: string) {
+    const el = document.querySelector(`#${id}`)
+    if (el) {
+      (el as HTMLElement).scrollIntoView({ behavior: 'smooth' })
+    }
+  }
 </script>
 
 <style scoped>
