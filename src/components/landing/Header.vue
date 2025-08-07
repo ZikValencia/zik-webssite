57436f75e9abfa899160208c97ea550efce8303c
exe123deadstart <valencia.exequiel123@gmail.com>
2025-08-07 22:42:34 +0900

    update: Updated header

--- src/components/landing/Header.vue
+++ src/components/landing/Header.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="header-wrapper">
-    <!-- Content over the wave -->
     <v-container class="text-center">
       <v-img class="mb-4" height="300" src="@/assets/zik-logo.svg" />
       <v-col cols="12">
         <v-btn
           v-for="item in ['Home','Skills', 'Experience', 'Projects', 'More About Me','Contact']"
           :key="item"
-          class="mx-2 text-h4"
+          class="mx-2 text-h5"
           size="x-large"
           :to="`/${item.toLowerCase()}`"
           variant="text"
@@ -27,20 +26,5 @@
 </script>
 
 <style scoped>
-.header-wrapper {
-  position: relative;
-  overflow: hidden;
-}
-.wavy-bg {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 320px;
-  z-index: 0;
-}
-.v-container {
-  position: relative;
-  z-index: 1;
-}
+
 </style>
